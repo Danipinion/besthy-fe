549a11b16adda9573e0abf0e2aeb423646d38003
Mohammad Dani Taufiqurrohman <mlymikro@gmail.com>
2024-07-26 21:42:07 +0700

    ⚙️:fix url

--- src/instance.ts
+++ src/instance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3002",
-  // baseURL: "https://besthy-be.vercel.app",
+  // baseURL: "http://localhost:3002",
+  baseURL: "https://besthy-be.vercel.app",
 });
 
 export default instance;
